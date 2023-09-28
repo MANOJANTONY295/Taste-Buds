30acb0c09654d4df3c1c1b2e7211f1e60021bcac
manojantony295@gmail.com <manojantony295@gmail.com>
2023-09-28 22:13:06 +0530

    security

--- Taste_buds/settings.py
+++ Taste_buds/settings.py
@@ -137,5 +137,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'manojantony@gmail.com'
-EMAIL_HOST_PASSWORD = 'AtoZ@123'
+EMAIL_HOST_PASSWORD = 'rwkm fttn hcqi ebgm'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
